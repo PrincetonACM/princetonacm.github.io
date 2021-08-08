39c63c423dc30baf06a203aaa4ff6da6269325bf
Nalin <nranjan38@gmail.com>
2021-08-08 00:09:48 -0400

    added announcements section

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -2,6 +2,28 @@ $(".full img").on("click", function() {
   $(this).toggleClass("zoom");
 });
 
+$('.announcement-slider').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		prevArrow: $('.prev-arrow-announcement'),
+		nextArrow: $('.next-arrow-announcement')
+	});
+
+$('.events').slick({
+		dots: false,
+		arrows: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		prevArrow: $('.prev-arrow-events'),
+		nextArrow: $('.next-arrow-events')
+	});
+
 (function($) {
 
   $.fn.menumaker = function(options) {
